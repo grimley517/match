729d2daa7d25935f1d134163d025f32487b7d230
Brian Jones <ba.jones@me.com>
2014-11-13 10:23:45 +1100

    Initial Commit

---
+++ matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  matchismo
+//
+//  Created by Brian on 13/11/2014.
+//  Copyright (c) 2014 Grimsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  matchismo
+//
+//  Created by Brian on 13/11/2014.
+//  Copyright (c) 2014 Grimsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
